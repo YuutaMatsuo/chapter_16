f56fd5effa89e53585ce23f107ec56a8788b879f
user <user@192.168.1.45>
2023-12-19 16:48:03 +0900

    12/19 6限目

--- src/p01/Main.java
+++ src/p01/Main.java
@@ -1,7 +1,14 @@
 package p01;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-
+		// コレクション名<データ型> 変数名 = new コレクション名<データ型>();
+		ArrayList<String> sampleList = new ArrayList<String>();
+		
+		// 注意点 (基本データ型は指定できない)
+//		ArrayList<int> sampleList = new ArrayList<int>();
+		
 	}
 }
